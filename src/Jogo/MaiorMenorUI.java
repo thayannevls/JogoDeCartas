802ec7f0650f102996c0b924fcc3abbe44a7cb7e
Thayanne Luiza <thayannevls@gmail.com>
2015-08-04 15:45:59 -0300

    Update MaiorMenorUI.java
    Implementação da interface.

--- src/Jogo/MaiorMenorUI.java
+++ src/Jogo/MaiorMenorUI.java
@@ -1,5 +1,37 @@
 package Jogo;
 
-public class MaiorMenorUI {
+import java.util.Scanner;
 
+public class MaiorMenorUI {
+	
+	/*
+	 * Metodo para receber a carta
+	 */
+	/*public static Carta recebeCarta(S){
+		Scanner sc = new Scanner(System.in);
+		String chute = sc.nextLine();
+	} */
+	
+	public static void maiorOuMenor(Carta cartaSecreta){
+		Scanner sc = new Scanner(System.in);
+		String chute = sc.nextLine();
+		
+		while(chute != "sair") {
+		
+			Carta cartaChute = new Carta(Carta.toValor(chute));
+			if(cartaSecreta.equalsValor(cartaChute)){
+				System.out.println("Acertou. A carta secreta era " + cartaSecreta.toString());
+				chute = "sair";
+			}
+			else{
+				System.out.println( cartaChute.maiorMenor(cartaSecreta) );
+				chute = sc.nextLine();
+			}
+			
+		}
+		sc.close();
+	}
+	
+	
+	
 }
